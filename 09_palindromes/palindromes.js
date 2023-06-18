9cf276ab97de71f5850adad792b29d7456efb26b
Asim <112316210+awsimness@users.noreply.github.com>
2023-06-18 16:50:45 +0500

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (strom) {
+    const s1=strom.toLowerCase().replace(/[^a-z]/g, "");
+    return s1.split('').reverse().join('')==s1;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
